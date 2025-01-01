54cce6fdee01c922f3cc9cecbfa08c72b2277069
yawenyao <y_yawen@163.com>
2025-01-02 00:57:18 +0800

    1

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import uView from 'uview-ui';
 import Vuex from 'vuex';
 import store from './store'; // 确保 store 文件存在且正确导出 Vuex 的 store 实例
 
+
 // 确保在开发环境下首先导入 Mock.js
 if (process.env.NODE_ENV === 'development') {
 	import('./utils/mock.js');
