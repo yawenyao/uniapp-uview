4cae2ef305944c7f163f81611962f2ffc5cb0057
yawenyao <y_yawen@163.com>
2025-01-04 23:02:26 +0800

    1

---
+++ locales/en.js
@@ -0,0 +1,20 @@
+export default {
+    login: {
+        username: 'Username',
+        password: 'Password',
+        loginBtn: 'Login',
+        required: 'Please enter {field}',
+        langMenu: {
+            zh: 'Chinese',
+            en: 'English'
+        }
+    },
+	home: {
+	        stockIn: 'Stock In Management',
+	        stockOut: 'Stock Out Management',
+	        scanStockIn: 'Scan Stock In',
+	        scanStockOut: 'Scan Stock Out',
+	        productList: 'Product List',
+	        mine: 'Personal Center'
+	    },
+};
---
+++ locales/zh.js
@@ -0,0 +1,20 @@
+export default {
+    login: {
+        username: '用户名',
+        password: '密码',
+        loginBtn: '登录',
+        required: '请输入{field}',
+        langMenu: {
+            zh: '中文',
+            en: '英文'
+        }
+    },
+    home: {
+        stockIn: '入库管理',
+        stockOut: '出库管理',
+        scanStockIn: '扫码入库',
+        scanStockOut: '扫码出库',
+        productList: '产品列表',
+        mine: '个人中心'
+    },
+};
